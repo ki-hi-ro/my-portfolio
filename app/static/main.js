d6326d692f63936ed7644a479b32c8dbb7174f07
ki-hi-ro <hiroki.hiroki@icloud.com>
2026-06-04 09:25:31 +0900

    Feat : ページトップボタン

---
+++ app/static/main.js
@@ -0,0 +1,16 @@
+const pageTopBtn = document.getElementById("page-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    pageTopBtn.classList.add("show");
+  } else {
+    pageTopBtn.classList.remove("show");
+  }
+});
+
+pageTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
